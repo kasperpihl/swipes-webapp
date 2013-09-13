7900284ee443cfd751dab59dac66cab336ce0b01
Jens Ahrengot Boddum <ahrengot@gmail.com>
2013-09-13 16:43:22 +0200

    Add test for selection toggling on todo list item views

--- test/spec/test.coffee
+++ test/spec/test.coffee
@@ -1,10 +1,48 @@
-describe "Basics", ->
-	it "App should be up and running", ->
-		expect( window.app ).to.exist
 
-describe "To Do list items", ->
-	describe "Selection", ->
-		it "should do something", ->
-			console.log "Doing something..."
+require ["jquery", "underscore", "backbone"], ($, _, Backbone) ->
+	
+	contentHolder = $("#content-holder")
+	
+	helpers = 
+		getListItemModel: ->
+			title: "Follow up on Martin"
+			order: 0
+			schedule: new Date()
+			completionDate: null
+			repeatOption: "never"
+			repeatDate: null
+			tags: ["work", "client"]
+			notes: ""
+		renderTodoList: (data) ->
+			dfd = new $.Deferred()
+			require ["text!templates/todo-list.html"], (ListTempl) ->
+				tmpl = _.template ListTempl
+				contentHolder.html tmpl( data )
+				dfd.resolve()
+
+			return dfd.promise()
+
+	describe "Basics", ->
+		it "App should be up and running", ->
+			expect( window.app ).to.exist
+
+	require ["model/ToDoModel", "view/list/DesktopListItem"], (Model, View) ->
+		describe "List Item Views", ->
+			
+			describe "Selection", ->
+				it "Should toggle selection when clicked", ->
+					model = new Model helpers.getListItemModel()
+					helpers.renderTodoList( items: [model.toJSON()] ).then ->
+						el = $("#content-holder .todo > li").first()
+						view = new View { el, model }
+
+						el.click()
+
+						expect( model.get "selected" ).to.be.true
+						expect( el.hasClass "selected" ).to.be.true
+
+						contentHolder.empty()
+
+
 
 			
--- test/spec/test.js
+++ test/spec/test.js
@@ -1,14 +1,59 @@
 (function() {
-  describe("Basics", function() {
-    return it("App should be up and running", function() {
-      return expect(window.app).to.exist;
+  require(["jquery", "underscore", "backbone"], function($, _, Backbone) {
+    var contentHolder, helpers;
+    contentHolder = $("#content-holder");
+    helpers = {
+      getListItemModel: function() {
+        return {
+          title: "Follow up on Martin",
+          order: 0,
+          schedule: new Date(),
+          completionDate: null,
+          repeatOption: "never",
+          repeatDate: null,
+          tags: ["work", "client"],
+          notes: ""
+        };
+      },
+      renderTodoList: function(data) {
+        var dfd;
+        dfd = new $.Deferred();
+        require(["text!templates/todo-list.html"], function(ListTempl) {
+          var tmpl;
+          tmpl = _.template(ListTempl);
+          contentHolder.html(tmpl(data));
+          return dfd.resolve();
+        });
+        return dfd.promise();
+      }
+    };
+    describe("Basics", function() {
+      return it("App should be up and running", function() {
+        return expect(window.app).to.exist;
+      });
     });
-  });
-
-  describe("To Do list items", function() {
-    return describe("Selection", function() {
-      return it("should do something", function() {
-        return console.log("Doing something...");
+    return require(["model/ToDoModel", "view/list/DesktopListItem"], function(Model, View) {
+      return describe("List Item Views", function() {
+        return describe("Selection", function() {
+          return it("Should toggle selection when clicked", function() {
+            var model;
+            model = new Model(helpers.getListItemModel());
+            return helpers.renderTodoList({
+              items: [model.toJSON()]
+            }).then(function() {
+              var el, view;
+              el = $("#content-holder .todo > li").first();
+              view = new View({
+                el: el,
+                model: model
+              });
+              el.click();
+              expect(model.get("selected")).to.be["true"];
+              expect(el.hasClass("selected")).to.be["true"];
+              return contentHolder.empty();
+            });
+          });
+        });
       });
     });
   });
