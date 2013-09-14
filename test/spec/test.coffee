4c95abc08a4d555c00ad529f36ed028165be3f2a
Jens Ahrengot Boddum <ahrengot@gmail.com>
2013-09-14 23:47:56 +0200

    Refactor tests after todo list refactoring and flush out hover tests (They dont work yet)

--- test/spec/test.coffee
+++ test/spec/test.coffee
@@ -1,32 +1,61 @@
 
-require ["jquery", "underscore", "backbone"], ($, _, Backbone) ->
+require [
+	"jquery", 
+	"underscore", 
+	"backbone"
+	], ($, _, Backbone) ->
 	
 	contentHolder = $("#content-holder")
 	
 	helpers = 
-		getListItemModel: ->
-			title: "Follow up on Martin"
-			order: 0
-			schedule: new Date()
-			completionDate: null
-			repeatOption: "never"
-			repeatDate: null
-			tags: ["work", "client"]
-			notes: ""
-		renderTodoList: (data) ->
+		getListItemModels: ->
+			[
+					title: "Follow up on Martin"
+					order: 0
+					schedule: new Date()
+					completionDate: null
+					repeatOption: "never"
+					repeatDate: null
+					tags: ["work", "client"]
+					notes: ""
+				,
+					title: "Dummy task #3"
+					order: 2
+					schedule: new Date()
+					completionDate: null
+					repeatOption: "never"
+					repeatDate: null
+					tags: ["work", "client"]
+					notes: ""
+				,
+					title: "Dummy task #2"
+					order: 1
+					schedule: new Date("October 13, 2013 11:13:00")
+					completionDate: null
+					repeatOption: "never"
+					repeatDate: null
+					tags: ["work", "client"]
+					notes: ""
+				,
+					title: "Dummy task #4"
+					order: 3
+					schedule: new Date("September 18, 2013 16:30:02")
+					completionDate: null
+					repeatOption: "never"
+					repeatDate: null
+					notes: ""
+			]
+		renderTodoList: ->
 			dfd = new $.Deferred()
-			require ["text!templates/todo-list.html"], (ListTempl) ->
+			require ["text!templates/todo-list.html", "model/ToDoModel", "view/list/DesktopListItem"], (ListTempl, Model, View) ->
 				tmpl = _.template ListTempl
 
-				data = 
-					taskGroups: [
-						{
-							deadline: "Tomorrow"
-							tasks: [helpers.getListItemModel()] 
-						}
-					]
+				data = { title: "Tomorrow" }
 
 				contentHolder.html $("<ol class='todo'></ol>").append( tmpl data )
+
+
+
 				dfd.resolve()
 
 			return dfd.promise()
@@ -45,43 +74,76 @@ require ["jquery", "underscore", "backbone"], ($, _, Backbone) ->
 		describe "List Item model", ->
 			model = new Model()
 
-			it "Should set scheduleStr when instantiated", ->
+			it "Should create scheduleStr property when instantiated", ->
 				expect( model.get("scheduleString") ).to.equal "past"
 			
 			it "Should update scheduleStr when schedule property is changed", ->
 				date = model.get "schedule"
 
 				# unset for change event to occur
-				model.set( "schedule", "" )
+				model.unset "schedule"
 				
 				date.setDate date.getDate()+1
 				model.set( "schedule", date )
 
 				expect( model.get("scheduleString") ).to.equal "Tomorrow"
 
+			it "Should create timeStr property when model is instantiated", ->
+				expect( model.get("timeStr") ).to.exist
+
+			it "Should update timeStr when schedule property is changed", ->
+				timeBeforeChange = model.get "timeStr"
+				
+				date = model.get "schedule"
+				# Unset because its an object and wont trigger a change if we just update the object itself.
+				model.unset "schedule"
+
+				date.setHours date.getHours() - 1
+				model.set( "schedule", date )
+				
+				timeAfterChange = model.get "timeStr"
+				
+				expect( timeBeforeChange ).to.not.equal timeAfterChange
+
 	#
 	# To Do View
 	#
-	require ["model/ToDoModel", "view/list/DesktopListItem"], (Model, View) ->
+	require ["collection/ToDoCollection", "model/ToDoModel", "view/list/DesktopListItem"], (ToDoCollection, Model, View) ->
 		describe "List Item View", ->
-			
-			describe "Selection", ->
-				it "Should toggle selection when clicked", ->
-					model = new Model helpers.getListItemModel()
-					helpers.renderTodoList( items: [model.toJSON()] ).then ->
-						el = $("#content-holder .todo ol > li").first()
-						view = new View { el, model }
+		
+			helpers.renderTodoList().then ->
+				model = new Model helpers.getListItemModels()[0]
+				view = new View { model }
+				
+				describe "Selection", ->
 
-						el.click()
-
-						expect( el ).to.not.be.empty
+					contentHolder.find(".todo ol").append view.el
+					view.$el.click()
+				
+					it "Should toggle selected property on model when clicked", ->
 						expect( model.get "selected" ).to.be.true
-						expect( el.hasClass "selected" ).to.be.true
+					
+					it "Should toggle selected class on element when clicked", ->
+						expect( view.$el.hasClass "selected" ).to.be.true
 
-						debugger
+				contentHolder.empty()
 
-						contentHolder.empty()
+			describe "Hovering", ->
+				todos = new ToDoCollection()
+				for model in helpers.getListItemModels()
+					todos.add model
 
+				helpers.renderTodoList().then ->
 
-
-			
+					it "Should trigger a 'allow-toggle-completed' event when hovering any task on the left edge", ->
+						# Lyt på event emits fra Backbone obj.
+						# expect(2).to.be.lessThan 1
+					it "Should trigger a 'allow-toggle-schedule' event when hovering any task on the right edge", ->
+						# Lyt på event emits fra Backbone obj.
+						# expect(2).to.be.lessThan 1
+					it "All views should listen for 'allow-toggle-completed' and 'allow-toggle-schedule' event and toggle if they are 'selected'", ->
+						# Lyt på event emits fra Backbone obj.
+						# expect(2).to.be.lessThan 1
+					it "All views should listen for 'allow-toggle-completed' and 'allow-toggle-schedule' event and toggle if they are the current hovered view, no matter if they are selected or not", ->
+						# Maybe compare e.currentTarget to self like if (selected is true or e.currentTarget is @)
+						# expect(2).to.be.lessThan 1
--- test/spec/test.js
+++ test/spec/test.js
@@ -3,31 +3,54 @@
     var contentHolder, helpers;
     contentHolder = $("#content-holder");
     helpers = {
-      getListItemModel: function() {
-        return {
-          title: "Follow up on Martin",
-          order: 0,
-          schedule: new Date(),
-          completionDate: null,
-          repeatOption: "never",
-          repeatDate: null,
-          tags: ["work", "client"],
-          notes: ""
-        };
+      getListItemModels: function() {
+        return [
+          {
+            title: "Follow up on Martin",
+            order: 0,
+            schedule: new Date(),
+            completionDate: null,
+            repeatOption: "never",
+            repeatDate: null,
+            tags: ["work", "client"],
+            notes: ""
+          }, {
+            title: "Dummy task #3",
+            order: 2,
+            schedule: new Date(),
+            completionDate: null,
+            repeatOption: "never",
+            repeatDate: null,
+            tags: ["work", "client"],
+            notes: ""
+          }, {
+            title: "Dummy task #2",
+            order: 1,
+            schedule: new Date("October 13, 2013 11:13:00"),
+            completionDate: null,
+            repeatOption: "never",
+            repeatDate: null,
+            tags: ["work", "client"],
+            notes: ""
+          }, {
+            title: "Dummy task #4",
+            order: 3,
+            schedule: new Date("September 18, 2013 16:30:02"),
+            completionDate: null,
+            repeatOption: "never",
+            repeatDate: null,
+            notes: ""
+          }
+        ];
       },
-      renderTodoList: function(data) {
+      renderTodoList: function() {
         var dfd;
         dfd = new $.Deferred();
-        require(["text!templates/todo-list.html"], function(ListTempl) {
-          var tmpl;
+        require(["text!templates/todo-list.html", "model/ToDoModel", "view/list/DesktopListItem"], function(ListTempl, Model, View) {
+          var data, tmpl;
           tmpl = _.template(ListTempl);
           data = {
-            taskGroups: [
-              {
-                deadline: "Tomorrow",
-                tasks: [helpers.getListItemModel()]
-              }
-            ]
+            title: "Tomorrow"
           };
           contentHolder.html($("<ol class='todo'></ol>").append(tmpl(data)));
           return dfd.resolve();
@@ -44,41 +67,65 @@
       return describe("List Item model", function() {
         var model;
         model = new Model();
-        it("Should set scheduleStr when instantiated", function() {
+        it("Should create scheduleStr property when instantiated", function() {
           return expect(model.get("scheduleString")).to.equal("past");
         });
-        return it("Should update scheduleStr when schedule property is changed", function() {
+        it("Should update scheduleStr when schedule property is changed", function() {
           var date;
           date = model.get("schedule");
-          model.set("schedule", "");
+          model.unset("schedule");
           date.setDate(date.getDate() + 1);
           model.set("schedule", date);
           return expect(model.get("scheduleString")).to.equal("Tomorrow");
         });
+        it("Should create timeStr property when model is instantiated", function() {
+          return expect(model.get("timeStr")).to.exist;
+        });
+        return it("Should update timeStr when schedule property is changed", function() {
+          var date, timeAfterChange, timeBeforeChange;
+          timeBeforeChange = model.get("timeStr");
+          date = model.get("schedule");
+          model.unset("schedule");
+          date.setHours(date.getHours() - 1);
+          model.set("schedule", date);
+          timeAfterChange = model.get("timeStr");
+          return expect(timeBeforeChange).to.not.equal(timeAfterChange);
+        });
       });
     });
-    return require(["model/ToDoModel", "view/list/DesktopListItem"], function(Model, View) {
+    return require(["collection/ToDoCollection", "model/ToDoModel", "view/list/DesktopListItem"], function(ToDoCollection, Model, View) {
       return describe("List Item View", function() {
-        return describe("Selection", function() {
-          return it("Should toggle selection when clicked", function() {
-            var model;
-            model = new Model(helpers.getListItemModel());
-            return helpers.renderTodoList({
-              items: [model.toJSON()]
-            }).then(function() {
-              var el, view;
-              el = $("#content-holder .todo ol > li").first();
-              view = new View({
-                el: el,
-                model: model
-              });
-              el.click();
-              expect(el).to.not.be.empty;
-              expect(model.get("selected")).to.be["true"];
-              expect(el.hasClass("selected")).to.be["true"];
-              debugger;
-              return contentHolder.empty();
+        helpers.renderTodoList().then(function() {
+          var model, view;
+          model = new Model(helpers.getListItemModels()[0]);
+          view = new View({
+            model: model
+          });
+          describe("Selection", function() {
+            contentHolder.find(".todo ol").append(view.el);
+            view.$el.click();
+            it("Should toggle selected property on model when clicked", function() {
+              return expect(model.get("selected")).to.be["true"];
             });
+            return it("Should toggle selected class on element when clicked", function() {
+              return expect(view.$el.hasClass("selected")).to.be["true"];
+            });
+          });
+          return contentHolder.empty();
+        });
+        return describe("Hovering", function() {
+          var model, todos, _i, _len, _ref;
+          todos = new ToDoCollection();
+          _ref = helpers.getListItemModels();
+          for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+            model = _ref[_i];
+            todos.add(model);
+          }
+          return helpers.renderTodoList().then(function() {
+            it("Should trigger a 'allow-toggle-completed' event when hovering any task on the left edge", function() {});
+            it("Should trigger a 'allow-toggle-schedule' event when hovering any task on the right edge", function() {});
+            it("All views should listen for 'allow-toggle-completed' and 'allow-toggle-schedule' event and toggle if they are 'selected'", function() {});
+            return it("All views should listen for 'allow-toggle-completed' and 'allow-toggle-schedule' event and toggle if they are the current hovered view, no matter if they are selected or not", function() {});
           });
         });
       });
