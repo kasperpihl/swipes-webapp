3f3426481e5139a32e8eef127baa2805e8d0ab4a
Jens Ahrengot Boddum <ahrengot@gmail.com>
2013-09-18 22:50:16 +0200

    If any todo doesn't have a set order, calculate the orders and re-render the list

--- app/scripts/coffee/view/todo.coffee
+++ app/scripts/coffee/view/todo.coffee
@@ -1,7 +1,42 @@
-define ["view/List"], (ListView) ->
+###
+
+	# Libraries
+	1. https://github.com/farhadi/html5sortable/blob/master/jquery.sortable.js
+	2. GreenSock Draggable (Fucking lækkert!) - http://greensock.com/draggable/
+
+	# Kasper
+	1. https://github.com/kasperpihl/swipes-ios/blob/master/Swipes/Classes/Models/CustomClasses/KPToDo.m#L184
+	2. https://github.com/kasperpihl/swipes-ios/blob/master/Swipes/Classes/Handlers/ItemHandler.m#L71
+
+
+###
+
+define ["underscore", "view/List", "controller/ListSortController"], (_, ListView, ListSortController) ->
 	ListView.extend
 		sortTasks: (tasks) ->
 			return _.sortBy tasks, (model) -> model.get("order")
 		groupTasks: (tasksArr) ->
 			tasksArr = @sortTasks tasksArr
 			return [ { deadline: "Tasks", tasks: tasksArr } ]
+		setTodoOrder: (todos) ->
+			takenPositions = ( m.get( "order" ) for m in todos when m.has "order" )
+			pushOrderCount = 0
+
+			for view, i in @subviews when !view.model.has "order"
+
+				# If position is taken, set order to next available position
+				while _.contains( takenPositions, i + pushOrderCount )
+					pushOrderCount++
+				
+				view.model.set( "order", i + pushOrderCount )
+
+			@renderList()
+		afterRenderList: (todos) ->
+			# If we find any todos without a defined order,
+			# determine its correct order and re-render the list
+			return @setTodoOrder( todos ) if _.any( todos, (m) -> not m.has "order" )
+			
+			# Alright, by now all todos have a set order. Continue on ...
+			@sortController.destroy() if @sortController?
+
+			# @sortController = new ListSortController(@$el, )
--- app/scripts/view/todo.js
+++ app/scripts/view/todo.js
@@ -1,5 +1,17 @@
+/*
+
+	# Libraries
+	1. https://github.com/farhadi/html5sortable/blob/master/jquery.sortable.js
+	2. GreenSock Draggable (Fucking lækkert!) - http://greensock.com/draggable/
+
+	# Kasper
+	1. https://github.com/kasperpihl/swipes-ios/blob/master/Swipes/Classes/Models/CustomClasses/KPToDo.m#L184
+	2. https://github.com/kasperpihl/swipes-ios/blob/master/Swipes/Classes/Handlers/ItemHandler.m#L71
+*/
+
+
 (function() {
-  define(["view/List"], function(ListView) {
+  define(["underscore", "view/List", "controller/ListSortController"], function(_, ListView, ListSortController) {
     return ListView.extend({
       sortTasks: function(tasks) {
         return _.sortBy(tasks, function(model) {
@@ -14,6 +26,43 @@
             tasks: tasksArr
           }
         ];
+      },
+      setTodoOrder: function(todos) {
+        var i, m, pushOrderCount, takenPositions, view, _i, _len, _ref;
+        takenPositions = (function() {
+          var _i, _len, _results;
+          _results = [];
+          for (_i = 0, _len = todos.length; _i < _len; _i++) {
+            m = todos[_i];
+            if (m.has("order")) {
+              _results.push(m.get("order"));
+            }
+          }
+          return _results;
+        })();
+        pushOrderCount = 0;
+        _ref = this.subviews;
+        for (i = _i = 0, _len = _ref.length; _i < _len; i = ++_i) {
+          view = _ref[i];
+          if (!(!view.model.has("order"))) {
+            continue;
+          }
+          while (_.contains(takenPositions, i + pushOrderCount)) {
+            pushOrderCount++;
+          }
+          view.model.set("order", i + pushOrderCount);
+        }
+        return this.renderList();
+      },
+      afterRenderList: function(todos) {
+        if (_.any(todos, function(m) {
+          return !m.has("order");
+        })) {
+          return this.setTodoOrder(todos);
+        }
+        if (this.sortController != null) {
+          return this.sortController.destroy();
+        }
       }
     });
   });
