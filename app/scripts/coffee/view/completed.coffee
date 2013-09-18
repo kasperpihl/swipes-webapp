dad5895028b143cb9e0a1ab28488a8d6e0e6e315
Jens Ahrengot Boddum <ahrengot@gmail.com>
2013-09-18 15:27:58 +0200

    Remove log msg

--- app/scripts/coffee/view/completed.coffee
+++ app/scripts/coffee/view/completed.coffee
@@ -1,5 +1,4 @@
 define ["view/List"], (ListView) ->
 	ListView.extend
 		getListItems: ->
-			console.log "Completed: ", swipy.todos.getCompleted()
 			return swipy.todos.getCompleted()
--- app/scripts/view/completed.js
+++ app/scripts/view/completed.js
@@ -2,7 +2,6 @@
   define(["view/List"], function(ListView) {
     return ListView.extend({
       getListItems: function() {
-        console.log("Completed: ", swipy.todos.getCompleted());
         return swipy.todos.getCompleted();
       }
     });
