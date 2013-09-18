732a892d6823423b97fbba137add2ef1500c234a
Jens Ahrengot Boddum <ahrengot@gmail.com>
2013-09-18 17:20:20 +0200

    Make custom task sorting and grouping for completed view

--- app/scripts/coffee/view/completed.coffee
+++ app/scripts/coffee/view/completed.coffee
@@ -1,4 +1,10 @@
 define ["view/List"], (ListView) ->
 	ListView.extend
+		sortTasks: (tasks) ->
+			return _.sortBy( tasks, (model) -> model.get( "schedule" ).getTime() ).reverse()
+		groupTasks: (tasksArr) ->
+			tasksArr = @sortTasks tasksArr
+			tasksByDate = _.groupBy( tasksArr, (m) -> m.get "completionStr" )
+			return ( { deadline, tasks } for deadline, tasks of tasksByDate )
 		getListItems: ->
 			return swipy.todos.getCompleted()
--- app/scripts/view/completed.js
+++ app/scripts/view/completed.js
@@ -1,6 +1,30 @@
 (function() {
   define(["view/List"], function(ListView) {
     return ListView.extend({
+      sortTasks: function(tasks) {
+        return _.sortBy(tasks, function(model) {
+          return model.get("schedule").getTime();
+        }).reverse();
+      },
+      groupTasks: function(tasksArr) {
+        var deadline, tasks, tasksByDate;
+        tasksArr = this.sortTasks(tasksArr);
+        tasksByDate = _.groupBy(tasksArr, function(m) {
+          return m.get("completionStr");
+        });
+        return (function() {
+          var _results;
+          _results = [];
+          for (deadline in tasksByDate) {
+            tasks = tasksByDate[deadline];
+            _results.push({
+              deadline: deadline,
+              tasks: tasks
+            });
+          }
+          return _results;
+        })();
+      },
       getListItems: function() {
         return swipy.todos.getCompleted();
       }
