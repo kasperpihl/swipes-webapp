28431ea58bf2bd650e112e7f2013a4e6adb3306c
Jens Ahrengot Boddum <ahrengot@gmail.com>
2013-10-04 12:25:35 +0200

    Add sidebar tags dynamically using real tags

--- app/scripts/coffee/view/sidebar/TagFilter.coffee
+++ app/scripts/coffee/view/sidebar/TagFilter.coffee
@@ -3,7 +3,8 @@ define ["underscore", "backbone"], (_, Backbone) ->
 		events: 
 			"click li": "toggleFilter"
 		initialize: ->
-			console.log "New Tag Filter view created"
+			@listenTo( swipy.tags, "add remove reset", @render, @ )
+			@render()
 		toggleFilter: (e) ->
 			tag = e.currentTarget.innerText
 			el = $( e.currentTarget ).toggleClass "selected"
@@ -13,5 +14,25 @@ define ["underscore", "backbone"], (_, Backbone) ->
 			else
 				Backbone.trigger( "remove-filter", "tag", tag )
 		render: ->
+			list = @$el. find ".rounded-tags"
+			list.empty()
 			
+			@renderTag tag, list for tag in swipy.tags.models
+			@renderTagInput list
+
+			return @el
+		renderTag: (tag, list) ->
+			list.append "<li>#{ tag.get 'title' }</li>" 
+		renderTagInput: (list) ->
+			list.append "
+				<li class='tag-input'>
+					<form class='add-tag'>
+						<input type='text' placeholder='Add new tag'>
+					</form>
+				</li>"
+		remove: ->
+			@stopListening()
+			@undelegateEvents()
+			@$el.remove()
+
 
--- app/scripts/view/sidebar/TagFilter.js
+++ app/scripts/view/sidebar/TagFilter.js
@@ -5,17 +5,41 @@
         "click li": "toggleFilter"
       },
       initialize: function() {
-        return console.log("New Tag Filter view created");
+        this.listenTo(swipy.tags, "add remove reset", this.render, this);
+        return this.render();
       },
       toggleFilter: function(e) {
         var el, tag;
         tag = e.currentTarget.innerText;
         el = $(e.currentTarget).toggleClass("selected");
         if (el.hasClass("selected")) {
-          return console.log("Filter for ", tag);
+          return Backbone.trigger("apply-filter", "tag", tag);
         } else {
-          return console.log("De-filter for ", tag);
+          return Backbone.trigger("remove-filter", "tag", tag);
         }
+      },
+      render: function() {
+        var list, tag, _i, _len, _ref;
+        list = this.$el.find(".rounded-tags");
+        list.empty();
+        _ref = swipy.tags.models;
+        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+          tag = _ref[_i];
+          this.renderTag(tag, list);
+        }
+        this.renderTagInput(list);
+        return this.el;
+      },
+      renderTag: function(tag, list) {
+        return list.append("<li>" + (tag.get('title')) + "</li>");
+      },
+      renderTagInput: function(list) {
+        return list.append("				<li class='tag-input'>					<form class='add-tag'>						<input type='text' placeholder='Add new tag'>					</form>				</li>");
+      },
+      remove: function() {
+        this.stopListening();
+        this.undelegateEvents();
+        return this.$el.remove();
       }
     });
   });
