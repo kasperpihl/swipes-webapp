ff0947013d9388afd09a2a375527dffe2c158e1d
Jens Ahrengot Boddum <ahrengot@gmail.com>
2013-10-09 10:29:31 +0200

    Toggle open/close sections when clicking button

--- app/scripts/coffee/view/settings/Snoozes.coffee
+++ app/scripts/coffee/view/settings/Snoozes.coffee
@@ -1,5 +1,9 @@
 define ["view/settings/BaseSubview", "text!templates/settings-snoozes.html"], (BaseView, Tmpl) ->
 	BaseView.extend
 		className: "snoozes"
+		events:
+			"click button": "toggleSection"
 		setTemplate: ->
 			@template = _.template Tmpl
+		toggleSection: (e) ->
+			$(e.currentTarget.parentNode.parentNode).toggleClass "toggled"
--- app/scripts/view/settings/Snoozes.js
+++ app/scripts/view/settings/Snoozes.js
@@ -2,8 +2,14 @@
   define(["view/settings/BaseSubview", "text!templates/settings-snoozes.html"], function(BaseView, Tmpl) {
     return BaseView.extend({
       className: "snoozes",
+      events: {
+        "click button": "toggleSection"
+      },
       setTemplate: function() {
         return this.template = _.template(Tmpl);
+      },
+      toggleSection: function(e) {
+        return $(e.currentTarget.parentNode.parentNode).toggleClass("toggled");
       }
     });
   });
