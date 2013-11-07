a6b88dbafbbd404b602def13aa443ec2903586ce
Jens Ahrengot Boddum <ahrengot@gmail.com>
2013-11-07 13:28:12 +0100

    Update tasks so there's a better distribution between scheduled, active and completed — And make titles more appropriate

--- app/scripts/DebugHelper.js
+++ app/scripts/DebugHelper.js
@@ -19,88 +19,98 @@
             completionDate: new Date("September 02, 2013 11:51:34"),
             repeatOption: "never",
             repeatDate: null,
-            tags: ["work", "client"],
+            tags: ["Work", "Newbizz"],
             notes: ""
           }, {
-            title: "Make visual research",
+            title: "Make visual research for new Swipes design",
             order: 1,
-            schedule: new Date("October 13, 2013 11:13:00"),
-            completionDate: null,
+            schedule: new Date("September 13, 2013 09:00:00"),
+            completionDate: new Date("September 13, 2013 11:13:00"),
             repeatOption: "never",
             repeatDate: null,
             notes: ""
           }, {
-            title: "Buy a new Helmet",
+            title: "Buy a new MBT Helmet",
             order: 2,
-            schedule: new Date("March 1, 2017 16:30:02"),
+            schedule: null,
             completionDate: null,
             repeatOption: "never",
             repeatDate: null,
-            tags: ["Personal", "work", "Bike", "Outside"],
+            tags: ["Personal", "Outside"],
             notes: ""
           }, {
-            title: "Renew Wired Magazine subscription",
+            title: "Introduce the team to new MailChimp",
             schedule: new Date("September 17, 2013 20:30:02"),
             completionDate: null,
             repeatOption: "never",
-            tags: ["presentation", "work"],
+            tags: ["Presentation", "Work"],
             repeatDate: null,
             notes: ""
           }, {
-            title: "Clean up the house",
+            title: "Find a weekend getaway on Kayak.com",
             order: 0,
             schedule: new Date("September 16, 2013 22:30:02"),
             completionDate: null,
             repeatOption: "never",
             repeatDate: null,
-            tags: ["Errand", "City", "work"],
+            tags: ["Adventure"],
             notes: ""
           }, {
-            title: "Buy a biiiiiiiike!!!",
-            schedule: new Date("September 17, 2013 23:59:02"),
+            title: "Prepare the Marketing Workshop",
+            order: 1,
+            schedule: new Date("September 17, 2013 09:00:00"),
             completionDate: null,
-            repeatOption: "never",
-            repeatDate: null,
-            notes: ""
-          }, {
-            title: "Check that insurance covers bike",
-            schedule: new Date("September 17, 2013 23:59:02"),
-            completionDate: null,
-            repeatOption: "never",
-            repeatDate: null,
-            tags: ["Ahrengotti Del Barista e Assasino", "work"],
-            notes: ""
-          }, {
-            title: "Build webapp",
-            schedule: new Date("September 17, 2013 23:59:02"),
-            completionDate: new Date("July 15, 2013 11:51:34"),
-            repeatOption: "never",
-            repeatDate: null,
-            tags: ["Ahrengotti Del Barista e Assasino", "work"],
-            notes: ""
-          }, {
-            title: "Learn to make Tiramisú",
-            schedule: new Date("September 17, 2013 23:59:02"),
-            completionDate: new Date("September 18, 2013 09:51:34"),
-            repeatOption: "never",
-            repeatDate: null,
-            tags: ["Food", "Die hipsters"],
-            notes: ""
-          }, {
-            title: "Learn who Martin is",
-            schedule: new Date("September 17, 2013 23:59:02"),
-            completionDate: new Date("September 2, 2013 09:51:34"),
             repeatOption: "never",
             repeatDate: null,
             tags: ["Work"],
             notes: ""
           }, {
-            title: "Dirt Jumps, Dirt Jumps, Dirt Jumps!",
-            schedule: new Date("September 17, 2013 23:59:02"),
+            title: "Check that insurance covers bike",
+            schedule: new Date("December 1, 2013 09:00:00"),
             completionDate: null,
             repeatOption: "never",
             repeatDate: null,
-            tags: ["Random tag 2", "work"],
+            tags: ["Chore"],
+            notes: ""
+          }, {
+            title: "Finalize Swipes Web App",
+            schedule: new Date("November 29, 2013 09:00:00"),
+            completionDate: null,
+            repeatOption: "never",
+            repeatDate: null,
+            tags: ["Work", "HTML5", "Backbone.js"],
+            notes: "Faster, better, stronger — http://swipesapp.com/"
+          }, {
+            title: "Learn to make Tiramisú",
+            schedule: null,
+            completionDate: null,
+            repeatOption: "never",
+            repeatDate: null,
+            tags: ["Food", "Personal"],
+            notes: "There are some great recipes on Epicurious.com!"
+          }, {
+            title: "Look up stock prices for LinkedIn",
+            schedule: new Date("November 22, 2013 09:00:00"),
+            completionDate: null,
+            repeatOption: "never",
+            repeatDate: null,
+            tags: ["Hustling", "Work"],
+            notes: "There are some great recipes on Epicurious.com!"
+          }, {
+            title: "Pack suitcase for Dublin",
+            schedule: new Date("October 27, 2013 18:00:00"),
+            completionDate: new Date("October 27, 2013 22:00:34"),
+            repeatOption: "never",
+            repeatDate: null,
+            tags: ["Work"],
+            notes: ""
+          }, {
+            title: "Prepare pitch for The Web Summit",
+            schedule: new Date("October 27, 2013 10:00:00"),
+            completionDate: new Date("October 27, 2013 14:59:02"),
+            repeatOption: "never",
+            repeatDate: null,
+            tags: ["Presentaion", "Work"],
             notes: ""
           }
         ];
