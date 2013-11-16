94ec509d8045f53e8daf2f89574566f325e32157
Jens Ahrengot Boddum <ahrengot@gmail.com>
2013-11-16 02:36:03 +0100

    Copy login folder and script to dist folder when building

--- Gruntfile.js
+++ Gruntfile.js
@@ -308,6 +308,8 @@ module.exports = function (grunt) {
                     src: [
                         '*.{ico,png,txt}',
                         '.htaccess',
+                        'login/*.*',
+                        'scripts/Login.js',
                         'images/{,*/}*.{webp,gif}',
                         'styles/fonts/{,*/}*.*'
                     ]
