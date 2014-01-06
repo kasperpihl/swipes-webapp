20c27468f7d276d41fd62b48373c0836d2b6262e
Jens Ahrengot Boddum <ahrengot@gmail.com>
2014-01-06 17:44:41 +0100

    Ignore modernizr task. It breaks build

--- Gruntfile.js
+++ Gruntfile.js
@@ -396,7 +396,7 @@ module.exports = function (grunt) {
         'concat',
         'cssmin',
         'uglify',
-        'modernizr',
+        // 'modernizr',
         'copy:dist',
         'rev',
         'usemin'
