179df75c146a9bf458b67e066fe115685ffb2aa2
iremlopsum <kristjanfou@gmail.com>
2015-10-30 16:04:56 +0200

    inbox app update

--- app/scripts/plugins/apps/inbox-app.js
+++ app/scripts/plugins/apps/inbox-app.js
@@ -1,1 +1,21 @@
-alert("working");
+var checkbox = $('.checkbox');
+var check = $('.check');
+
+checkbox.on('click', function() {
+	if ($(this).children(check).hasClass('checked')) {
+		$(this).children(check).removeClass('checked');
+	} else {
+		$(this).children(check).addClass('checked');
+	}
+})
+
+$('.more-checkboxes').on('click', function() {
+	console.log('main');
+	if ($('.dropdown').hasClass('dropped')) {
+		console.log('if');		
+		$('.dropdown').removeClass('dropped');
+	} else {
+		console.log('else');	
+		$('.dropdown').addClass('dropped');		
+	}
+})
