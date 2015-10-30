978309f7d018d24875e7eb155bf36b799cd86de0
Kasper Pihl Tornøe <kasper@pihl.it>
2015-10-30 19:01:09 +0200

    Fixing iframe and refactoring

--- app/apps/inbox/script.js
+++ app/apps/inbox/script.js
@@ -1,24 +1,23 @@
-alert('asfg')
-
-
+var $ = window.jQuery;
 var checkbox = $('.checkbox');
 var check = $('.check');
 
-checkbox.on('click', function() {
-	if ($(this).children(check).hasClass('checked')) {
-		$(this).children(check).removeClass('checked');
-	} else {
-		$(this).children(check).addClass('checked');
-	}
+$(document).ready(function(){
+	checkbox.on('click', function() {
+		if ($(this).children(check).hasClass('checked')) {
+			$(this).children(check).removeClass('checked');
+		} else {
+			$(this).children(check).addClass('checked');
+		}
+	})
+
+	$('.more-checkboxes').on('click', function() {
+		if ($('.dropdown').hasClass('dropped')) {	
+			$('.dropdown').removeClass('dropped');
+		} else {	
+			$('.dropdown').addClass('dropped');		
+		}
+	})
 })
 
-$('.more-checkboxes').on('click', function() {
-	console.log('main');
-	if ($('.dropdown').hasClass('dropped')) {
-		console.log('if');		
-		$('.dropdown').removeClass('dropped');
-	} else {
-		console.log('else');	
-		$('.dropdown').addClass('dropped');		
-	}
-})
+
